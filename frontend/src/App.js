b7a99e16661835a5aa6bad84ba99aebeeeb5b243
Meghana Kankipati <kankipatimeghana23@gmail.com>
2024-04-11 15:07:40 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-
+import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      hello react
+
+
+      <Header />
+      <Home />
     </div>
   );
 }
